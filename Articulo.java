ca9dd4080e5236debcd127eed9a868b3d34adf5d
Pinox084 <114494657+Pinox084@users.noreply.github.com>
2022-09-27 11:11:30 -0300

    Update Articulo.java

--- Articulo.java
+++ Articulo.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package tarea1;
 
-/**
- *
- * @author Nicolás
- */
 public class Articulo {
+    private float peso;
+    private String nombre;
+    private String descripcion;
+    private float precio;
     
+    public Articulo(float kg, String alias, String que, float caro){
+        peso = kg;
+        nombre = alias;
+        descripcion = que;
+        precio = caro;
+    }
+    
+    public float getPrecio(){
+        return precio;
+    }
+    public float getPeso(){
+        return peso;
+    }
+            
 }
