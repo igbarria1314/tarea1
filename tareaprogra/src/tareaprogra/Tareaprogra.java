9b2bb74d9db9b5ac432dc6a94c5f896ed71fed25
Nicolás Pino Leal <nicoandrespino@gmail.com>
2022-09-28 23:41:39 -0300

    Orden

---
+++ tareaprogra/src/tareaprogra/Tareaprogra.java
@@ -0,0 +1,17 @@
+
+package tareaprogra;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class Tareaprogra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
