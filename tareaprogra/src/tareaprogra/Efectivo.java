e4ed7ea35a5517962a1b161c80d6320a65f6e16b
Pinox084 <114494657+Pinox084@users.noreply.github.com>
2022-10-04 11:51:59 -0300

    Actualización y renombre .java

--- tareaprogra/src/tareaprogra/Efectivo.java
+++ tareaprogra/src/tareaprogra/Efectivo.java
@@ -2,16 +2,15 @@ package tareaprogra;
 
 import java.util.Date;
 public class Efectivo extends Pago{
-    private float pagoDelCliente;
-    public Efectivo(float p,float m, Date f){
-        super(m,f);
-        pagoDelCliente= p;
+    public Efectivo(float p, Date f){
+        super(p,f);
     }
     public float calcDevolucion(float p){
         
         if(p<0){
             p = p *-1;
         }
+        System.out.println("Devolucion:" + p + "\n");
         return p;
     }
 }
