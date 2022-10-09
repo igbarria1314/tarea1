58d596a7bdb2037b5d0f8836d6d67d9e66f92fc4
Pinox084 <114494657+Pinox084@users.noreply.github.com>
2022-10-08 22:33:33 -0300

    Create Boleta.java

---
+++ tareaprogra/src/tareaprogra/Boleta.java
@@ -0,0 +1,8 @@
+package tareaprogra;
+import java.util.Date;
+
+public class Boleta extends DocumentoTributario {
+     public Boleta(String dato, String carnet, Date calen){
+        super(dato,carnet,calen);
+    }
+}
