533c2bc08420e0bc994781d309aa23520f43f18f
Pinox084 <114494657+Pinox084@users.noreply.github.com>
2022-10-08 22:34:57 -0300

    Update Direccion.java

--- tareaprogra/src/tareaprogra/Direccion.java
+++ tareaprogra/src/tareaprogra/Direccion.java
@@ -1,15 +1,17 @@
 package tareaprogra;
 
-import java.util.Date;
 public class Direccion{
     private String direccion;
     public Direccion(String d){
         direccion=d;
     }
-    public getDireccion(){
+    public String getDireccion(){
         return direccion;
     }
     public String ToString(){
         return "Direccion:" + direccion;
     }
+
+    
 }
+
