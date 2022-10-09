c238bf7519cc6a3fba6e173cdb56582b48360771
Pinox084 <114494657+Pinox084@users.noreply.github.com>
2022-10-08 22:32:30 -0300

    Create Factura.java

---
+++ tareaprogra/src/tareaprogra/Factura.java
@@ -0,0 +1,8 @@
+package tareaprogra;
+import java.util.Date;
+
+public class Factura extends DocumentoTributario {
+    public Factura(String dato, String carnet, Date calen){
+        super(dato,carnet,calen);
+    }
+}
